e0e162162d4ca3deed2be5b5b0fe9105e6fc03f9
Yogesh Tyagi <tyagiyogesh848@gmail.com>
2025-07-02 15:21:41 +0530

    changed sever.js and package.json for production

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import { connectDB } from './db/connectDB.js';
 import routes from './routes/route.js';
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;  
+const __dirname = path.resolve();
 
 app.use(cors({
     origin:"http://localhost:5173",
@@ -20,6 +22,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/',routes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
